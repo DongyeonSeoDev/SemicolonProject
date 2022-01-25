6a5c7e96b2124587c9be9aee0111bd7ee6feda49
Firstund <yjw040608@gmail.com>
2022-01-25 21:03:02 +0900

    플레이어 총알의 SortingLayer를 Player로 변경, 에러 수정

--- Assets/Scripts/SlimeScripts/Player/PlayerDrainCollider.cs
+++ Assets/Scripts/SlimeScripts/Player/PlayerDrainCollider.cs
@@ -10,19 +10,19 @@ public class PlayerDrainCollider : MonoBehaviour
     [SerializeField]
     private float drainTime = 0.5f;
     private float drainTimer = 0f;
-    
-    private void OnEnable() 
+
+    private void OnEnable()
     {
         drainTimer = drainTime;
     }
 
     void Update()
     {
-        if(drainTimer > 0f)
+        if (drainTimer > 0f)
         {
             drainTimer -= Time.deltaTime;
 
-            if(drainTimer <= 0f)
+            if (drainTimer <= 0f)
             {
                 gameObject.SetActive(false);
             }
@@ -30,15 +30,19 @@ public class PlayerDrainCollider : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(canDrainObjLayers.CompareGameObjectLayer(other.gameObject))
+        if (canDrainObjLayers.CompareGameObjectLayer(other.gameObject))
         {
             // Debug.Log(other.gameObject.layer);
             // Drain되는 오브젝트는 삭제처리
-            
-            other.GetComponent<Enemy.Enemy>().EnemyDestroy();
+            Enemy.Enemy enemy = other.GetComponent<Enemy.Enemy>();
 
-            SlimeEventManager.TriggerEvent("OnDrain", other.gameObject.name, 1); // 여기의 param은 임시 값
-            Debug.Log("Do Drain");
-        }    
+            if (enemy != null)
+            {
+                enemy.EnemyDestroy();
+
+                SlimeEventManager.TriggerEvent("OnDrain", other.gameObject.name, 1); // 여기의 param은 임시 값
+                Debug.Log("Do Drain");
+            }
+        }
     }
 }
