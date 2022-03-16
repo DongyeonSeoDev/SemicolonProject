f417366f89e9b42e16f9a2b7698cf6f46e4d4291
DongyeonSeo <seodye04@gmail.com>
2022-03-16 20:46:40 +0900

    적 총알 이펙트 추가

--- Assets/Scripts/EnemyScripts/Effect/EnemyEffect.cs
+++ Assets/Scripts/EnemyScripts/Effect/EnemyEffect.cs
@@ -7,7 +7,6 @@ namespace Enemy
         private SpriteRenderer spriteRenderer = null;
         private Animator animator = null;
         private readonly int hashAnimationStart = Animator.StringToHash("AnimationStart");
-        private bool isPlaying = false;
 
         private void Awake()
         {
@@ -20,11 +19,8 @@ namespace Enemy
             spriteRenderer.color = color;
             animator.SetTrigger(hashAnimationStart);
 
-            isPlaying = true;
-
             Util.DelayFunc(() =>
             {
-                isPlaying = false;
                 gameObject.SetActive(false);
             }, 0.7f, this);
         }
