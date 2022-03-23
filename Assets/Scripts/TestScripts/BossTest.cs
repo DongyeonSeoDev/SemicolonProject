21b5033a0b42f2bfa8932b2eac11ebf850bf8a78
DongyeonSeo <seodye04@gmail.com>
2022-03-24 00:12:44 +0900

    적 스폰 에디터 수정

--- Assets/Scripts/TestScripts/BossTest.cs
+++ Assets/Scripts/TestScripts/BossTest.cs
@@ -1,64 +1,8 @@
-using UnityEditor;
 using UnityEngine;
 
 public class BossTest : MonoBehaviour
 {
+#if ENABLE_UNITYEVENTS
     public Vector3 playerStartPosition;
-}
-
-[CustomEditor(typeof(BossTest))]
-public class BossTestInspector : Editor
-{
-    public override void OnInspectorGUI()
-    {
-        base.OnInspectorGUI();
-
-        GUILayout.Space(10);
-
-        if (GUILayout.Button("Boss Spawn"))
-        {
-            if (Application.isPlaying && FindObjectOfType<Enemy.Boss1SkeletonKing>() == null)
-            {
-                SlimeGameManager.Instance.CurrentPlayerBody.transform.position = FindObjectOfType<BossTest>().playerStartPosition;
-                EventManager.TriggerEvent("SpawnEnemy", "BossTest_01");
-            }
-        }
-
-        GUILayout.Space(5);
-    }
-}
-
-public class BossTestWindow : EditorWindow
-{
-    private GUILayoutOption[] buttonLayout = new GUILayoutOption[]
-    {
-        GUILayout.Width(300),
-        GUILayout.Height(30)
-    };
-
-    [MenuItem("EnemyWindow/BossTest")]
-    static void Init()
-    {
-        GetWindow(typeof(BossTestWindow));
-    }
-
-    public void OnGUI()
-    {
-        GUILayout.FlexibleSpace();
-        GUILayout.BeginHorizontal();
-        GUILayout.FlexibleSpace();
-
-        if (GUILayout.Button("Boss Spawn", buttonLayout))
-        {
-            if (Application.isPlaying && FindObjectOfType<Enemy.Boss1SkeletonKing>() == null)
-            {
-                SlimeGameManager.Instance.CurrentPlayerBody.transform.position = FindObjectOfType<BossTest>().playerStartPosition;
-                EventManager.TriggerEvent("SpawnEnemy", "BossTest_01");
-            }
-        }
-
-        GUILayout.FlexibleSpace();
-        GUILayout.EndHorizontal();
-        GUILayout.FlexibleSpace();
-    }
+#endif
 }
