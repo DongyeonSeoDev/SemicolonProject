70caf0be3565e4d7ffcb68cfd2a749804b285efd
Firstund <yjw040608@gmail.com>
2022-03-28 13:38:57 +0900

    OrderInLayerControll 스크립트 수정

--- Assets/Scripts/Controller/OrderInLayerConroller.cs
+++ Assets/Scripts/Controller/OrderInLayerConroller.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[RequireComponent(typeof(SpriteRenderer))]
+using UnityEngine.U2D;
+
 public class OrderInLayerConroller : MonoBehaviour
 {
     private SpriteRenderer spriteRenderer = null;
+    private SpriteShapeRenderer spriteShapeRenderer = null;
     private readonly int offest = 20;
 
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if(spriteRenderer == null)
+        {
+            spriteShapeRenderer = GetComponent<SpriteShapeRenderer>();
+        }
     }
 
     void Update()
@@ -18,6 +25,13 @@ public class OrderInLayerConroller : MonoBehaviour
     }
     private void SetOrderInLayer()
     {
+        if(spriteRenderer == null)
+        {
+            spriteShapeRenderer.sortingOrder = offest - (int)Mathf.Round(transform.position.y);
+
+            return;
+        }
+        
         spriteRenderer.sortingOrder = offest - (int)Mathf.Round(transform.position.y);
     }
 }
