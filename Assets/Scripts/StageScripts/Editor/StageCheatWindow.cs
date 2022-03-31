cdea269bff4c5dfe8641cacbb3f3f77ffcf78672
gonTest <hsangsu40@gmail.com>
2022-03-31 21:54:11 +0900

    치트 에디터 간단 기능 하는중

--- Assets/Scripts/StageScripts/Editor/StageCheatWindow.cs
+++ Assets/Scripts/StageScripts/Editor/StageCheatWindow.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class StageCheatWindow : EditorWindow
 {
-    
+    public Player SlimePlayer => SlimeGameManager.Instance.Player;
 
-    [MenuItem("StageWindow/Cheat")]
+    private Stat playerStat = new Stat();
+    private int recoveryHp;
+    private StateAbnormality sa;
+
+    [MenuItem("Cheat/Normal Cheat")]
     public static void ShowEnemySpawnWindow()
     {
         GetWindow(typeof(StageCheatWindow));
@@ -16,7 +20,7 @@ public class StageCheatWindow : EditorWindow
     {
         if(StageManager.Instance.IsStageClear)
         {
-            UIManager.Instance.RequestSystemMsg("이미 문 열려있잖아 임마");
+            UIManager.Instance.RequestSystemMsg("이미 문 열려있다");
             return;
         }
 
@@ -25,9 +29,9 @@ public class StageCheatWindow : EditorWindow
         Enemy.EnemyManager.Instance.PlayerDeadEvent();
     }
 
-    private void Awake()
+    private void OnEnable()
     {
-        Debug.Log("1");
+        playerStat = SlimePlayer.PlayerStat;
     }
 
     private void OnGUI()
@@ -48,10 +52,50 @@ public class StageCheatWindow : EditorWindow
 
         GUILayout.Space(15);
 
-        if (GUILayout.Button("Apply Player Stat"))
-        {
+        playerStat.eternalStat.maxHp = EditorGUILayout.FloatField("Max Hp", playerStat.eternalStat.maxHp);
+        playerStat.eternalStat.minDamage = EditorGUILayout.IntField("Min Damage", playerStat.eternalStat.minDamage);
+        playerStat.eternalStat.maxDamage = EditorGUILayout.IntField("Max Damage", playerStat.eternalStat.maxDamage);
+        playerStat.eternalStat.defense = EditorGUILayout.IntField("Defense", playerStat.eternalStat.defense);
+        playerStat.eternalStat.speed = EditorGUILayout.FloatField("Speed", playerStat.eternalStat.speed);
 
+        GUILayout.Space(10);
+
+        if (GUILayout.Button("Apply Player Eternal Stat"))
+        {
+            SlimePlayer.PlayerStat = playerStat;
         }
+
+        GUILayout.Space(15);
+
+        recoveryHp = EditorGUILayout.IntField("Heal Current Hp n%", 20);
+
+        GUILayout.Space(7);
+
+        if (GUILayout.Button("Player Heal"))
+        {
+            SlimePlayer.GetHeal(recoveryHp);
+        }
+
+        GUILayout.Space(20);
+
+        GUILayout.BeginHorizontal();
+        sa = (StateAbnormality)EditorGUILayout.EnumPopup("Imprecation", sa);
+        if (GUILayout.Button("Get Imprecation"))
+        {
+            if (sa != StateAbnormality.None)
+            {
+                StateManager.Instance.StartStateAbnormality(sa);
+                UIManager.Instance.RequestSystemMsg("저주 걸림");
+            }
+        }
+        GUILayout.EndHorizontal();
+        if (GUILayout.Button("Remove All Imprecation"))
+        {
+            StateManager.Instance.RemoveAllStateAbnormality();
+            UIManager.Instance.RequestSystemMsg("모든 저주 해제");
+        }
+
+        GUILayout.Space(20);
 
         EditorGUI.EndDisabledGroup();
     }
