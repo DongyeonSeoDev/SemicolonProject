ff14948322987a616cec94d1f98e851a4da2d802
Firstund <yjw040608@gmail.com>
2022-04-14 00:32:34 +0900

    적 Collider안에서 돌진하면 돌진데미지가 안들어가는 버그 수정

--- Assets/Scripts/SlimeScripts/Slime/BodyPointCrashCheckCollider.cs
+++ Assets/Scripts/SlimeScripts/Slime/BodyPointCrashCheckCollider.cs
@@ -36,7 +36,7 @@ public class BodyPointCrashCheckCollider : MonoBehaviour
     {
         EventManager.StopListening("BodyPointCrash", BodyPointCrash);
     }
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerStay2D(Collider2D other)
     {
         if (whatIsWall.CompareGameObjectLayer(other.gameObject))
         {
