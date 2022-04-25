6a6d0e90822781860326553cbf8a6b30f527a995
Firstund <yjw040608@gmail.com>
2022-04-25 23:58:41 +0900

    TutorialEnemy 체력바 관련 수정

--- Assets/Scripts/Tutorial/TutorialEnemy/TutorialEnemy.cs
+++ Assets/Scripts/Tutorial/TutorialEnemy/TutorialEnemy.cs
@@ -2,14 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 namespace Enemy
 {
     public abstract class TutorialEnemy : MonoBehaviour, ICanGetDamagableEnemy
     {
-        public Image hpBarFillImage; // 적 HP 바 채워진것중 체력 확인용 ( 없으면 UI 만들어야 함 ( Assets > Prefabs > EnemyPrefabs > EnemyUI 참고 ) )
-        public Image hpBarDamageFillImage; // 적 HP 바 채워진것중 데미지 확인용
-
         public int maxHP = 10;
         public int hp = 10;
 
