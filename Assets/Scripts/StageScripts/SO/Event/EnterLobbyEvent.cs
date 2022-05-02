4bc5a330b48c1fa0a330e8b0248bb3e4d0a7944d
gonTest <hsangsu40@gmail.com>
2022-05-03 08:39:37 +0900

    수정

--- Assets/Scripts/StageScripts/SO/Event/EnterLobbyEvent.cs
+++ Assets/Scripts/StageScripts/SO/Event/EnterLobbyEvent.cs
@@ -9,10 +9,10 @@ public class EnterLobbyEvent : MapEventSO
         {
             GameManager.Instance.savedData.tutorialInfo.isEnded = true;
             StageManager.Instance.SaveStage("Stage0-06");
-            CutsceneManager.Instance.PlayCutscene("TutorialCutscene2");
+            
         }
 
-        
+        CutsceneManager.Instance.PlayCutscene("TutorialCutscene2");
 
         StageManager.Instance.canNextStage = CanNextStage;
         StageManager.Instance.SetClearStage();
