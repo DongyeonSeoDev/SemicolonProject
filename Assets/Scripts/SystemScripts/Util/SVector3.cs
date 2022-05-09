b2d6fd57703b6a316d02f0f9a4a0c8c5405406fe
gonTest <hsangsu40@gmail.com>
2022-05-09 19:25:21 +0900

    맵 버그 수정, 랜덤 인카운터 로직 수정, 튜토 컷씬 수정

--- Assets/Scripts/SystemScripts/Util/SVector3.cs
+++ Assets/Scripts/SystemScripts/Util/SVector3.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public static class SVector3
 {
     public static Vector3 half => Vector3.one * 0.5f;
+    public static Vector3 two => Vector3.one * 2f;
     public static Vector3 onePointTwo => new Vector3(1.2f, 1.2f, 1.2f);
     public static Vector3 onePointThree => new Vector3(1.3f, 1.3f, 1.3f);
     public static Vector3 onePointSix => new Vector3(1.6f, 1.6f, 1.6f);
