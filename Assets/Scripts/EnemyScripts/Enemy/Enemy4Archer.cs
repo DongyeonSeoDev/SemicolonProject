dad468e96d54565b3989ddccb995b24f9bb3000d
DongyeonSeo <seodye04@gmail.com>
2022-05-14 22:50:39 +0900

    적 4 이동과 공격 제작중

--- Assets/Scripts/EnemyScripts/Enemy/Enemy4Archer.cs
+++ Assets/Scripts/EnemyScripts/Enemy/Enemy4Archer.cs
@@ -2,40 +2,37 @@ namespace Enemy
 {
     public class Enemy4Archer : Enemy
     {
-        // TODO: 공격 방식 정하고 코드 작성
-        //private EnemyCommand enemyAttackCommand;
-        //private EnemyCommand enemyAttackPlayerCommand;
+        private EnemyCommand enemyAttackCommand;
+        private EnemyCommand enemyAttackPlayerCommand;
 
         protected override void OnEnable()
         {
             base.OnEnable();
 
-            enemyData.isLongDistanceAttack = true;
             enemyData.chaseSpeed = 10f;
             enemyData.attackDelay = 1f;
-            enemyData.minRunAwayTime = 2f; // TODO : 값 설정
-            enemyData.maxRunAwayTime = 4f; // TODO : 값 설정
             enemyData.playerAnimationTime = 0.55f;
+            enemyData.isAttackPlayerDistance = 5f;
             enemyData.maxHP = 50;
             enemyData.hp = 50;
 
-            // TODO: 움직임 코드 작성해야함
-            //enemyData.enemyMoveCommand = new EnemyRandomMoveCommand(enemyData, positionCheckData); // MoveCommand 생성
-            //enemyData.enemySpriteRotateCommand = new EnemySpriteRotateCommand(enemyData);
-            //enemyAttackPlayerCommand = new EnemyAttackPlayerCommand(transform, this, enemyData.attackPower);
-            //enemyAttackCommand = new EnemyAttackCommand(this, enemyData.enemyObject.transform, enemyData.attackPower);
+            enemyData.enemyMoveCommand = new EnemyLongDistanceFollowPlayerCommand(enemyData, transform, rb, enemyData.chaseSpeed, enemyData.isAttackPlayerDistance);
+            enemyData.enemySpriteRotateCommand = new EnemySpriteRotateCommand(enemyData);
+
+            enemyAttackPlayerCommand = new EnemyAttackPlayerCommand(transform, this, enemyData.attackPower);
+            enemyAttackCommand = new EnemyAttackCommand(this, enemyData.enemyObject.transform, enemyData.attackPower);
         }
 
-        //public void EnemyAttack() // 애니메이션에서 실행 - 적 공격
-        //{
-        //    if (enemyData.eEnemyController == EnemyController.PLAYER)
-        //    {
-        //        enemyAttackPlayerCommand.Execute();
-        //    }
-        //    else if (enemyData.eEnemyController == EnemyController.AI)
-        //    {
-        //        enemyAttackCommand.Execute();
-        //    }
-        //}
+        public void EnemyAttack() // 애니메이션에서 실행 - 적 공격
+        {
+            if (enemyData.eEnemyController == EnemyController.PLAYER)
+            {
+                enemyAttackPlayerCommand.Execute();
+            }
+            else if (enemyData.eEnemyController == EnemyController.AI)
+            {
+                enemyAttackCommand.Execute();
+            }
+        }
     }
 }
