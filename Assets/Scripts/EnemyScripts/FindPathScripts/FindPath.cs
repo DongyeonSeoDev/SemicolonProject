63479510a7d7370d1aaa3727486e7b6fa9b02cf3
DongyeonSeo <seodye04@gmail.com>
2022-05-27 21:56:03 +0900

    스테이지를 bool 배열로 바꾸는 함수 추가

--- Assets/Scripts/EnemyScripts/FindPathScripts/FindPath.cs
+++ Assets/Scripts/EnemyScripts/FindPathScripts/FindPath.cs
@@ -3,21 +3,50 @@ using UnityEngine.Tilemaps;
 
 public class FindPath : MonoBehaviour
 {
-    public Tilemap tilemap;
+    public Tilemap[] tilemap;
+    public Vector2Int limitMinPosition;
+    public Vector2Int limitMaxPosition;
+
+    public bool[] stageData;
+    public int stageWidth;
+    public int offsetX;
+    public int offsetY;
 
     private void Start()
     {
-        BoundsInt cellBounds = tilemap.cellBounds;
+        SetStageData();
+    }
 
-        for (int i = cellBounds.min.x; i <= cellBounds.max.x; i++)
+    private void SetStageData()
+    {
+        ResetStageData();
+
+        for (int i = 0; i < tilemap.Length; i++)
         {
-            for (int j = cellBounds.min.y; j <= cellBounds.max.y; j++)
+            BoundsInt cellBounds = tilemap[i].cellBounds;
+
+            for (int j = cellBounds.min.x; j <= cellBounds.max.x; j++)
             {
-                if ((tilemap.GetTile(new Vector3Int(i, j, 0)) != null) || (tilemap.GetTile(new Vector3Int(i - 1, j, 0)) != null) || (tilemap.GetTile(new Vector3Int(i, j - 1, 0)) != null) || (tilemap.GetTile(new Vector3Int(i - 1, j - 1, 0)) != null))
+                for (int k = cellBounds.min.y; k <= cellBounds.max.y; k++)
                 {
-                    Debug.Log(new Vector2(i, j));
+                    if ((tilemap[i].GetTile(new Vector3Int(j, k, 0)) != null) || (tilemap[i].GetTile(new Vector3Int(j - 1, k, 0)) != null) || (tilemap[i].GetTile(new Vector3Int(j, k - 1, 0)) != null) || (tilemap[i].GetTile(new Vector3Int(j - 1, k - 1, 0)) != null))
+                    {
+                        stageData[GetBoolPosition(j, k)] = true;
+                    }
                 }
             }
         }
     }
+
+    private void ResetStageData()
+    {
+        offsetX = -limitMinPosition.x;
+        offsetY = -limitMinPosition.y;
+
+        stageWidth = limitMaxPosition.x - limitMinPosition.x + 1;
+
+        stageData = new bool[stageWidth * (limitMaxPosition.y - limitMinPosition.y + 1)];
+    }
+    
+    private int GetBoolPosition(int x, int y) => (x + offsetX) + (stageWidth * (y + offsetY));
 }
