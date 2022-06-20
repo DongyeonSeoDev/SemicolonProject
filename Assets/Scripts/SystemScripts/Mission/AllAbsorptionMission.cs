e613a74033d3c572504b94775147bdf24fd8ee92
gonTest <hsangsu40@gmail.com>
2022-06-21 01:05:01 +0900

    흡수 미션 적용

--- Assets/Scripts/SystemScripts/Mission/AllAbsorptionMission.cs
+++ Assets/Scripts/SystemScripts/Mission/AllAbsorptionMission.cs
@@ -8,13 +8,14 @@ public class AllAbsorptionMission : Mission
     }
     public override void End(bool breakDoor = false)
     {
-        
+        Enemy.EnemyManager.Instance.isOnlyAbsorption = false;
     }
 
     public override void Start()
     {
         isEnd = false;
         SlimeGameManager.Instance.PlayerBodyChange(Global.OriginBodyID);
+        Enemy.EnemyManager.Instance.isOnlyAbsorption = true;
     }
 
     public override void Update()
