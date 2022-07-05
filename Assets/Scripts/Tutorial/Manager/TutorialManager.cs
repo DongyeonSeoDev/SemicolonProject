f9bd0e6236812f0982242ef8883be772c361d938
gonTest <hsangsu40@gmail.com>
2022-07-05 19:36:07 +0900

    UI획득 아이콘 효과 제거

--- Assets/Scripts/Tutorial/Manager/TutorialManager.cs
+++ Assets/Scripts/Tutorial/Manager/TutorialManager.cs
@@ -371,19 +371,35 @@ public class TutorialManager : MonoSingleton<TutorialManager>
 
     private void GetInventoryUI() //ÀÎº¥ ¾òÀ½
     {
-        GetUIIcon(UIType.INVENTORY, ObjectManager.Instance.itrObjDic["Merchant"].transform.position);
+        //GetUIIcon(UIType.INVENTORY, ObjectManager.Instance.itrObjDic["Merchant"].transform.position);
+        Util.DelayFunc(() =>
+        {
+            KeyActionManager.Instance.GetElement(InitGainType.INVENTORY_UI);
+            UIOn(KeyAction.INVENTORY);
+            gm.savedData.userInfo.uiActiveDic[KeyAction.INVENTORY] = true;
+        }, 1, this); 
     }
 
     private void GetStatUI() //½ºÅÈ UI È¹µæ
     {
-        GetUIIcon(UIType.STAT, ObjectManager.Instance.itrObjDic["StatNPC"].transform.position);
-        
+        //GetUIIcon(UIType.STAT, ObjectManager.Instance.itrObjDic["StatNPC"].transform.position);
+        Util.DelayFunc(() =>
+        {
+            KeyActionManager.Instance.GetElement(InitGainType.STAT_UI);
+            UIOn(KeyAction.STAT);
+            gm.savedData.userInfo.uiActiveDic[KeyAction.STAT] = true;
+        }, 1, this);
     }
 
     private void GetMonsterCollectionUI() //¸ó½ºÅÍ µµ°¨ UI È¹µæ
     {
-        GetUIIcon(UIType.MONSTER_COLLECTION, ObjectManager.Instance.itrObjDic["MonsterCollectionObjNPC"].transform.position);
-        
+        //GetUIIcon(UIType.MONSTER_COLLECTION, ObjectManager.Instance.itrObjDic["MonsterCollectionObjNPC"].transform.position);
+        Util.DelayFunc(() =>
+        {
+            KeyActionManager.Instance.GetElement(InitGainType.MONCOL_UI);
+            UIOn(KeyAction.MONSTER_COLLECTION);
+            gm.savedData.userInfo.uiActiveDic[KeyAction.MONSTER_COLLECTION] = true;
+        }, 1, this);
     }
 
     public void GetBodyChangeSlot()  //º¯½Å ½½·Ô È¹µæ(2,3¹øÂ°)  
