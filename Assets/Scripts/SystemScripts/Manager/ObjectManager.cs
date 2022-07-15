18ec7cfdf54a6f43a80abd030c7adf9e9b6111b2
gonTest <hsangsu40@gmail.com>
2022-07-15 20:14:17 +0900

    ObjectManager 코드수정

--- Assets/Scripts/SystemScripts/Manager/ObjectManager.cs
+++ Assets/Scripts/SystemScripts/Manager/ObjectManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,5 +9,26 @@ public class ObjectManager : SingletonClass<ObjectManager>
     public void Reset()
     {
         itrObjDic.Clear();
+    }
+
+    public T GetObj<T>(string id) where T : InteractionObj
+    {
+        if (itrObjDic.ContainsKey(id))
+        {
+            try
+            {
+                T obj = itrObjDic[id] as T;
+                return obj;
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError(ex.Message);
+                Debug.LogWarning("해당 타입으로 변환 안됨 " + typeof(T).ToString());
+                return default;
+            }
+        }
+        
+        Debug.LogWarning("해당 아이디의 오브젝트가 존재하지 않음  ID : " + id);
+        return default(T);
     }
 }
