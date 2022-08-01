4c0288d31e4e7efd7ff210b4551f9fa92796007b
DongyeonSeo <seodye04@gmail.com>
2022-08-01 21:58:26 +0900

    적 6 데이터 설정

--- Assets/Scripts/EnemyScripts/Enemy/Enemy6Mage.cs
+++ Assets/Scripts/EnemyScripts/Enemy/Enemy6Mage.cs
@@ -2,6 +2,12 @@ namespace Enemy
 {
     public class Enemy6Mage : Enemy
     {
-        
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            enemyData.enemySpriteRotateCommand = new EnemySpriteRotateCommand(enemyData);
+            enemyData.enemyMoveCommand = new EnemyFollowPlayerCommand(enemyData, transform, rb, enemyData.chaseSpeed);
+        }
     }
 }
